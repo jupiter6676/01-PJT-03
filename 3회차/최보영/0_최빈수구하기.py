3723da5b43020b9878991b1942e7d592aa9bff11
jupiter6676 <jupiter6676@naver.com>
2022-07-29 15:46:14 +0900

    220729 01-PJT-03
    모의고사 1회

--- 3회차/최보영/0_최빈수구하기.py
+++ 3회차/최보영/0_최빈수구하기.py
@@ -1,3 +1,18 @@
 import sys
 
 sys.stdin = open("_최빈수구하기.txt")
+
+T = int(input())
+for _ in range(T):
+    test_case = int(input())
+    
+    scores = list(map(int, input().split()))
+    s_dict = dict()
+
+    for s in scores:
+        s_dict[s] = s_dict.get(s, 0) + 1
+
+    # 최빈수 내림차 → 점수 내림차
+    res = sorted(s_dict.items(), key=lambda x: (-x[1], -x[0]))
+    
+    print(f'#{test_case} {res[0][0]}')
--- 3회차/최보영/5_신용카드만들기2.py
+++ 3회차/최보영/5_신용카드만들기2.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_신용카드만들기2.txt")
+
+T = int(input())
+for test_case in range(1, T + 1):
+    card_nums = list(map(int, input().replace('-', '')))
+    
+    res = 0
+    if card_nums[0] == 3 or card_nums[0] == 4 or card_nums[0] == 5 or card_nums[0] == 6 or card_nums[0] == 9:
+        if len(card_nums) == 16:
+            res = 1
+    
+    print(f'#{test_case} {res}')
